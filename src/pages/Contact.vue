5124f7c43314b3e8b2fef033c0b3510dd8a35ae8
Chris Rault <chris@winefolly.com>
2019-08-05 18:18:40 +0200

    For reals this time

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,56 +1,14 @@
-<Layout class="bg-white">
-  <main class="p-10 relative z-10 bg-white mx-10 shadow-2xl">
-    <header>
-        <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">Contact</h1>
-        <p class="text-black text-lg sm:text-3xl">Got a project you'd like to discuss? Send a message to hello@mzango.com</p>
-        <hr class="border border-gray-200 mt-10 mb-10" style="width:510px;" />
-      </header>
-      <article class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto pb-10">
-        <h2 class="text-center md:text-2xl mt-20">Or say hello on social media</h2>
-        <nav class="my-2 text-center mb-10">
-          <a href="https://www.youtube.com/channel/UCA_zBs_qht33mT74LHD4Ptg" width="24" height="24" class="inline-block mr-3" target="_blank" rel="nofollow noopener"><g-image src="~/assets/youtube.png" alt="github" immediate="true" /></a>
-          <a href="https://instagram.com/thefoodiegardener" width="24" height="24" class="inline-block mr-3" target="_blank" rel="nofollow noopener"><g-image src="~/assets/instagram.png" alt="github" immediate="true" /></a>
-          <a href="https://twitter.com/connectr" width="24" height="24" class="inline-block mr-3" target="_blank" rel="nofollow noopener"><g-image src="~/assets/twitter.png" alt="github" immediate="true" /></a>
-          <a href="https://github.com/devignerforhire" width="24" height="24" class="inline-block mr-3" target="_blank" rel="nofollow noopener"><g-image src="~/assets/github.png" alt="github" immediate="true" /></a>
-        </nav>
-      </article>
+<template>
+  <Layout class="absolute top-0 left-0 right-0 bottom-0">
+    <main class="home h-full w-full absolute top-0">
     </main>
   </Layout>
 </template>
 
 <script>
-import config from '~/.temp/config.js'
-
 export default {
-  metaInfo () {
-    return {
-      title: 'Conact',
-      meta: [
-        {
-          key: 'description',
-          name: 'description',
-          content: ''
-        },
-
-        { property: "og:type", content: 'article' },
-        { property: "og:title", content:'Contact' },
-        { property: "og:description", content: '' },
-        { property: "og:url", content: `${this.config.siteUrl}/contact/` },
-        { property: "og:image", content: '/images/bleda-card.png' },
-
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:title", content: 'Contact' },
-        { name: "twitter:description", content: '' },
-        { name: "twitter:site", content: "@connectr" },
-        { name: "twitter:creator", content: "@connectr" },
-        { name: "twitter:image", content: '/images/bleda-card.png' },
-      ],
-    }
-  },
-  computed: {
-    config () {
-      return config
-    },
-  },
+  metaInfo: {
+    title: 'Contact'
+  }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,6 @@
 <template>
   <Layout class="absolute top-0 left-0 right-0 bottom-0">
     <main class="home h-full w-full absolute top-0">
-      <g-image src="~/assets/images/home-header-pink.jpg" class="object-cover h-full" />
     </main>
   </Layout>
 </template>
