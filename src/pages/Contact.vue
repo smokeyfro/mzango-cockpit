05afc5045cb86ab18bb29c7ac8ea65afbca3662a
Chris Rault <chris@winefolly.com>
2019-08-05 18:10:11 +0200

    Contact cleanup

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,12 +2,10 @@
   <main class="p-10 relative z-10 bg-white mx-10 shadow-2xl">
     <header>
         <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">Contact</h1>
-        <p class="text-black text-lg sm:text-3xl">Got a project you'd like to discuss?</p>
-        <p class="text-black text-lg">Fill out the form below and I'll get in touch asap.</p>
+        <p class="text-black text-lg sm:text-3xl">Got a project you'd like to discuss? Send a message to hello@mzango.com</p>
         <hr class="border border-gray-200 mt-10 mb-10" style="width:510px;" />
       </header>
       <article class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto pb-10">
-        <Contact />
         <h2 class="text-center md:text-2xl mt-20">Or say hello on social media</h2>
         <nav class="my-2 text-center mb-10">
           <a href="https://www.youtube.com/channel/UCA_zBs_qht33mT74LHD4Ptg" width="24" height="24" class="inline-block mr-3" target="_blank" rel="nofollow noopener"><g-image src="~/assets/youtube.png" alt="github" immediate="true" /></a>
@@ -22,12 +20,8 @@
 
 <script>
 import config from '~/.temp/config.js'
-import Contact from '@/components/Contact.vue'
 
 export default {
-  components: {
-    Contact
-  },
   metaInfo () {
     return {
       title: 'Conact',
