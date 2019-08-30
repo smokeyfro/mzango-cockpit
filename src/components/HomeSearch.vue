2e251ac05779bd392f60acd789f9de19beeeb2b8
Chris Rault <chris@winefolly.com>
2019-08-30 10:22:05 +0200

    Let's take a few steps back why don't we

--- src/components/HomeSearch.vue
+++ src/components/HomeSearch.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="search-wrapper w-1/2 pr-4 relative z-20">
       <form>
-        <input type="text" class="bg-white shadow-2xl focus:outline-none focus:shadow-outline rounded-full py-4 px-8 text-lg block w-full appearance-none leading-normal text-black placeholder:text-gray-300" placeholder="Type in the name of a South African town or city" v-model="search" v-on:keyup="getfilteredData">
+        <input type="text" class="bg-white shadow-2xl focus:outline-none focus:shadow-outline rounded-full py-4 px-8 text-lg block w-full appearance-none leading-normal text-black placeholder:text-gray-300" placeholder="Type in the name of a South African town or city">
       </form>
     </div>
 </template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -30,22 +30,20 @@
       <BottomNav />
       <Footer />
     </main>
-    <div>
-      <modal>
-        <template slot="header">
-          <h2>Greetings fellow earthling!</h2>
-        </template>
-        <template slot="body">
-          <p class="text-xl">This is the early preview of Mzango, <em>the unofficial guide to backpacking in South Africa</em>.</p> 
-          <p class="my-4">The site is still in active development and a lot will change before the official launch. <a href="#" v-on:click="modal = false" class="font-bold bg-yellow-200">Subscribe</a> to be notified when we launch.</p>
-        </template>
-        <template slot="footer">
-          <div class="mt-5">
-            <button v-on:click="modal = false" aria-label="Close modal" type="button" name="button" class="bg-black text-white py-2 px-4 rounded-full">Roger that!</button>
-          </div>
-        </template>
-      </modal>
-    </div>
+    <modal v-show="modal === true">
+      <template slot="header">
+        <h2>Greetings fellow earthling!</h2>
+      </template>
+      <template slot="body">
+        <p class="text-xl">This is the early preview of Mzango, <em>the unofficial guide to backpacking in South Africa</em>.</p> 
+        <p class="my-4">The site is still in active development and a lot will change before the official launch. <a href="#" v-on:click="modal = false" class="font-bold bg-yellow-200">Subscribe</a> to be notified when we launch.</p>
+      </template>
+      <template slot="footer">
+        <div class="mt-5">
+          <button v-on:click="modal = false" aria-label="Close modal" type="button" name="button" class="bg-black text-white py-2 px-4 rounded-full">Roger that!</button>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
